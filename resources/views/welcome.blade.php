8c19b71a61dc06e9d410eeb02381fc92d2c76898
Mehdi-ahd <nolanpatinde0@gmail.comgit congig --global user.name Mehdi-ahdgit config --global user.email nolanpatinde0@gmail.com>
2025-07-30 12:55:53 +0100

    Correction

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>École Jean Piaget 2 - Excellence Éducative au Bénin</title>
 
-    <link rel="icon" href="{{ asset('img/gestedu-logo.svg') }}" sizes="any">
+    <link rel="icon" href="{{ asset('img/gestedu-logo.svg') }}" sizes="any" type="image/x-icon">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
